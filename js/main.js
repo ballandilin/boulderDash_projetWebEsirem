6b6be4e73f6557e0d0ddc6e749dccbee0c25b15d
Nicolas Benoit <nb412296@ad.u-bourgogne.fr>
2022-03-21 17:10:39 +0100

    add grid

--- js/main.js
+++ js/main.js
@@ -1,40 +1,42 @@
+window.onload = () => {
 
 
 
-let getMap = () => {
-    fetch("../map/map.json")
-    .then(response => {
-       return response.json();
-    })
-    .then(jsondata => {
-        // console.log(jsondata);
-        // console.log(jsondata["niveau"][0]);
-        afficherMap(jsondata);
-        // return jsondata;
-    }); 
-}
-getMap();
-
-let afficherMap = (maps) => {
-
-    let grid = document.querySelector("grid");
-
-
-    maps["niveau"][0].forEach(element => {
-        var div = document.createElement("div");
-        element.forEach(_case => {
-            if (_case != null) {
-
-                var c = document.createElement("div");
-                c.classList.add("case");
-                c.innerHTML = _case;
-                div.appendChild(c);
-
-            } else {
-                div.appendChild(document.createElement("div"));
-            }
+    let getMap = () => {
+        fetch("../map/map.json")
+        .then(response => {
+           return response.json();
         })
-        grid.appendChild(div);
-        // console.table(element);
-    });
+        .then(jsondata => {
+            // console.log(jsondata);
+            // console.log(jsondata["niveau"][0]);
+            afficherMap(jsondata);
+            // return jsondata;
+        }); 
+    }
+    getMap();
+    
+    let afficherMap = (maps) => {
+    
+        let grid = document.querySelector("grid");
+    
+    
+        maps["niveau"][0].forEach(element => {
+            var div = document.createElement("div");
+            element.forEach(_case => {
+                if (_case != null) {
+    
+                    var c = document.createElement("div");
+                    c.classList.add("case");
+                    c.innerHTML = _case;
+                    div.appendChild(c);
+    
+                } else {
+                    div.appendChild(document.createElement("div"));
+                }
+            })
+            grid.appendChild(div);
+            // console.table(element);
+        });
+    }
 }
